abcfce6f6419366c841629434ef9ae51c4eed301
wagnerbrenner <wagner.brenner13@gmail.com>
2024-02-21 11:18:19 -0300

    Create table and pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,133 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Plus, Search, FileDown, MoreHorizontal } from "lucide-react";
+import { Header } from "./components/header";
+import { Tabs } from "./components/tabs";
+import { Button } from "./components/ui/button";
+import { Control, Input } from "./components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./components/ui/table";
+import { Pagination } from "./components/pagination";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export interface TagResponse {
+  first: number;
+  prev: number | null;
+  next: number;
+  last: number;
+  pages: number;
+  items: number;
+  data: Tag[];
 }
 
-export default App
+export interface Tag {
+  title: string;
+  slug: string;
+  amountOfVideos: number;
+  id: string;
+}
+
+export function App() {
+  const [searchParams] = useSearchParams();
+  const [filter, setFilter] = useState("");
+
+  const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1;
+
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ["get-tags", page],
+    queryFn: async () => {
+      const response = await fetch(
+        `http://localhost:3333/tags?_page=${page}&_per_page=10`
+      );
+      const data = await response.json();
+
+      return data;
+    },
+    placeholderData: keepPreviousData,
+    staleTime: 1000 * 60 * 5,
+  });
+
+  if (isLoading) {
+    return null;
+  }
+
+  return (
+    <div className="py-10 space-y-8 ">
+      <div>
+        <Header />
+        <Tabs />
+      </div>
+      <main className="max-w-6xl mx-auto space-y-5">
+        <div className="flex items-center gap-3">
+          <h1 className="text-xl font-bold">Tags</h1>
+          <Button variant="primary">
+            <Plus className="size-3" />
+            Create new
+          </Button>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <Input variant="filter">
+            <Search className="size-3" />
+            <Control
+              placeholder="Search tags..."
+              onChange={(event) => setFilter(event.target.value)}
+              value={filter}
+            />
+          </Input>
+          <Button>
+            <FileDown className="size-3" />
+            Export
+          </Button>
+        </div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead></TableHead>
+              <TableHead>Tag</TableHead>
+              <TableHead>Amount of Videos</TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {tagsResponse?.data.map((tag) => {
+              return (
+                <TableRow key={tag.id}>
+                  <TableCell></TableCell>
+                  <TableCell>
+                    <div className="flex flex-col gap-0.5">
+                      <span className="font-medium">{tag.title}</span>
+                      <span className="text-xs text-zinc-500">{tag.id}</span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="text-zinc-500">
+                    {tag.amountOfVideos} videos(s)
+                  </TableCell>
+                  <TableCell className="text-right">
+                    <Button size="icon">
+                      <MoreHorizontal className="size-4" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+
+        {tagsResponse && (
+          <Pagination
+            pages={tagsResponse.pages}
+            items={tagsResponse.items}
+            page={page}
+          />
+        )}
+      </main>
+    </div>
+  );
+}
